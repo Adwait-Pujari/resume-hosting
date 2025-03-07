7705e01ce25c53a1b0f57e88aebb753d8a769df3
Adwait Pujari <105132793+Adwait-Pujari@users.noreply.github.com>
2025-03-06 23:53:42 -0600

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://adwait-pujari.com"
+SITEURL = "https://adwait-pujari.github.io/resume-hosting/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
